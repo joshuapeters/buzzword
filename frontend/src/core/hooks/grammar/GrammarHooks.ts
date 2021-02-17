149cfee727b9de88f01a0879d3352df2652fd407
JP <jpeters@andculture.com>
2021-02-16 22:03:01 -0500

    started new hook

--- frontend/src/core/hooks/grammar/GrammarHooks.ts
+++ frontend/src/core/hooks/grammar/GrammarHooks.ts
@@ -1,13 +1,13 @@
-import tracery      from "tracery-grammar";
-import { useState } from "react";
+import tracery         from "tracery-grammar";
+import { useCallback } from "react";
 
 export function useGrammar() {
-    function getNewGrammar(grammarConfiguration: object, origin: string = "#origin#") {
+    const getNewGrammar = useCallback((grammarConfiguration: object, origin: string = "#origin#") =>  {
         const grammar = tracery.createGrammar(grammarConfiguration);
         grammar.addModifiers(tracery.baseEngModifiers);
 
         return fixINGModifier(grammar.flatten(origin));
-    }
+    }, []);
 
     return { getNewGrammar };
 }
