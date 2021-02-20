7317243a436b5e7347b55c1a19f84a112f422274
billygartrell <billygartrell@yahoo.com>
2021-02-20 13:49:45 -0500

    Further development of front-end stlying.

--- frontend/src/pages/Buzzword-HomePage.tsx
+++ frontend/src/pages/Buzzword-HomePage.tsx
@@ -21,33 +21,42 @@ export function BuzzwordHomePage() {
         <div>
             <Container fluid className="banner-top">
                 <Row>
-                    <Col><p>Coming Soon To Product Hunt 🚀</p></Col>
+                    <Col>
+                        <p>Coming Soon To Product Hunt 🚀</p>
+                    </Col>
                 </Row>
             </Container>
             <Container className="logo-container">
                 <Row>
-                    <Col><Image src={logo} />
+                    <Col>
+                        <Image src={logo} />
                     </Col>
                 </Row>
             </Container>
             <Container className="main-content">
                 <Row>
-                    <h1>{ buzzword }</h1>
+                    <Col className="headline" lg= {10} sm= {12}>
+                        <h1>{ buzzword }</h1>
+                    </Col>
                 </Row>
                 <Row>
-                    <p>Startup? 30-under-30-to-be? Use the button below to generate your next big mission statement that will make the world a better&nbsp;place.</p>
+                    <Col className="body-copy" lg= {8} sm={12}>
+                        <p>Startup? 30-under-30-to-be? Use the button below to generate your next big mission statement that will make the world a better&nbsp;place.</p>
+                    </Col>
                 </Row>
                 <Row>
-                    <Button onClick   = { () => setBuzzword(grammar.getNewGrammar(buzzword_grammar)) }
-                    size="lg" 
-                    variant="outline-primary"
-                    >Buzz It!</Button>
+                    <Col>
+                        <Button onClick   = { () => setBuzzword(grammar.getNewGrammar(buzzword_grammar)) }
+                        size="lg" 
+                        variant="outline-primary"
+                        >Buzz It!</Button>
+                    </Col> 
                 </Row>
             </Container>
-            <Container>
+            <Container className="footer">
                 <Row>
-                    <Col>Created by 🦌 Soft Animal Design &amp; Development</Col>
-                    <Col></Col>
+                    <Col lg={6}>Created by 🦌 &nbsp;<a href="#" title="Soft Animal Design and Development" target="_blank">Soft Animal Design &amp; Development</a></Col>
+                    <Col><a href='https://ko-fi.com/P5P23CVN6' target='_blank'><img className="coffee-button" src='https://cdn.ko-fi.com/cdn/kofi1.png?v=2' alt='Buy Me a Coffee at ko-fi.com' /></a></Col>
                 </Row>
             </Container>
         </div>
