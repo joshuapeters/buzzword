612fdcd2515be0a7ee9fd0e3f99da08598fecfc5
JP <jpeters@andculture.com>
2021-03-15 21:55:55 -0400

    added base typescript graphql api

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from 'apollo-server';
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
